b63c04eb5f2c450f1da32ee9da23de5fc344e329
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-08-19 11:31:58 +0700

    analysis order response to list

--- src/main/java/com/example/transactiontest/model/dto/ItemDto.java
+++ src/main/java/com/example/transactiontest/model/dto/ItemDto.java
@@ -1,10 +1,14 @@
 package com.example.transactiontest.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
 
     private String name;
--- src/main/java/com/example/transactiontest/service/TransactionService.java
+++ src/main/java/com/example/transactiontest/service/TransactionService.java
@@ -3,10 +3,8 @@ package com.example.transactiontest.service;
 import com.example.transactiontest.model.dto.*;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
@@ -26,7 +24,7 @@ public class TransactionService {
                     itemDto.setQuantity(dataInput.getOrderQuantity());
 
                     OrderDto orderDto = new OrderDto();
-                    orderDto.setId(dataInput.getOrderId());
+                    orderDto.setId(Long.valueOf(dataInput.getOrderId()));
                     List<ItemDto> itemDtos = new ArrayList<>();
                     itemDtos.add(itemDto);
                     orderDto.setItems(itemDtos);
@@ -34,7 +32,7 @@ public class TransactionService {
                     if(mapOutputs.get(dataInput.getEmail()) != null) {
                         boolean canGroup = false;
                         for(OrderDto order : mapOutputs.get(dataInput.getEmail()).getOrders()){
-                            if(order.getId() == dataInput.getOrderId()) {
+                            if(order.getId() == Long.valueOf(dataInput.getOrderId())) {
                                 order.getItems().add(itemDto);
                                 canGroup = true;
                                 break;
@@ -59,6 +57,36 @@ public class TransactionService {
         return mapOutputs;
     }
 
+    public Object convertTransactionValue_v2(TransactionRequest transactionRequest){
+
+        List<TransactionDetail> transactionDetails = transactionRequest.getTransactionDetails();
+
+        Map<String, List<TransactionDetail>> outputDatas = transactionDetails
+                .stream()
+                .collect(Collectors
+                        .groupingBy(
+                                TransactionDetail::getEmail
+                        )
+                );
+
+        List<OrderDto> orderDtos =
+                transactionDetails
+                        .stream()
+                        .map(element -> {
+                            OrderDto orderDto = new OrderDto();
+                            List<ItemDto> itemDtos = new ArrayList<>();
+                            ItemDto itemDto = new ItemDto(element.getProductName(), element.getProductCode(), element.getOrderQuantity());
+                            itemDtos.add(itemDto);
+                            orderDto.setId(element.getOrderId());
+                            orderDto.setItems(itemDtos);
+                            return orderDto;
+                        })
+                        .collect(Collectors.toList())
+                ;
+
+        return orderDtos;
+
+    }
 
 
 
